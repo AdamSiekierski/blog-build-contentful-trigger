84700eff0474cf14d74e44a645c79c5b58157dd4
AdamSiekierski <adam.siekiera@outlook.com>
2020-04-18 22:07:51 +0200

    Trigger the github dispatch

--- api/trigger.ts
+++ api/trigger.ts
@@ -1,6 +1,27 @@
 import { NowRequest, NowResponse } from '@now/node';
+import axios from 'axios';
 
 export default (req: NowRequest, res: NowResponse) => {
-  console.log(req);
-  res.status(200).send('Noice!');
+  axios
+    .post(
+      'https://api.github.com/repos/AdamSiekierski/blog/dispatches',
+      {
+        event_type: 'contentful-build',
+      },
+      {
+        headers: {
+          Authorization: `token ${process.env.GITHUB_TOKEN}`,
+        },
+      },
+    )
+    .then(({ data }) => {
+      console.log(data);
+      res.status(200).json({ message: 'Noice! it worked. maybe.' });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({
+        error: 'internal server error. fuck off',
+      });
+    });
 };
